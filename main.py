2ed0c23e52fd390b19f5cf411e2264c50a7c0463
Shay Bach <43345032+sasuke15@users.noreply.github.com>
2021-06-04 19:13:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+
+
+def nothing(x):
+    pass
+
+
+cap = cv2.VideoCapture(0)
+
+
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("L-H", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L-S", "Trackbars", 167, 255, nothing)
+cv2.createTrackbar("L-V", "Trackbars", 94, 255, nothing)
+cv2.createTrackbar("U-H", "Trackbars", 180, 180, nothing)
+cv2.createTrackbar("U-S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U-V", "Trackbars", 243, 255, nothing)
+
+font = cv2.FONT_HERSHEY_COMPLEX
+
+while True:
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    l_h = cv2.getTrackbarPos("L-H", "Trackbars")
+    l_S = cv2.getTrackbarPos("L-S", "Trackbars")
+    l_V = cv2.getTrackbarPos("L-V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U-H", "Trackbars")
+    u_S = cv2.getTrackbarPos("U-S", "Trackbars")
+    u_V = cv2.getTrackbarPos("U-V", "Trackbars")
+
+    lower_red = np.array([l_h, l_S, l_V])
+    upper_red = np.array([u_h, u_S, u_V])
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.erode(mask, kernel)
+
+    contours, hierachy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        approx = cv2.approxPolyDP(cnt, 0.02*cv2.arcLength(cnt, True), True)
+        x = approx.ravel()[0]
+        y = approx.ravel()[1]
+
+        if area > 400:
+            cv2.drawContours(frame, [approx], 0, (0, 0, 0), 5)
+
+            if len(approx) == 3:
+                cv2.putText(frame, "Triangle", (x, y), font, 1, (0, 0, 0))
+            elif len(approx) == 4:
+                cv2.putText(frame, "rectangle", (x, y), font, 1, (0, 0, 0))
+            elif 10 < len(approx) < 20:
+                cv2.putText(frame, "Circle", (x, y), font, 1, (0, 0, 0))
+
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+"""
+import cv2
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+while True:
+    success, img = cap.read()
+    cv2.imshow("Result", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+"""
